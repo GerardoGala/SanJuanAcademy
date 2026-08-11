b0d258e85502ac4da657388d5a6f2a5cef54d306
GerardoGala <ggala@hotmail.com>
2026-08-12 07:01:12 +0800

    link goes to blank page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document
+    .getElementById("learnMoreButton")
+    .addEventListener("click", () => {
+
+        document.getElementById("main").style.display = "none";
+
+        document.getElementById("courseFrame").style.display = "block";
+
+    });
