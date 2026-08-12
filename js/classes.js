2cd55c258121eb29c26d3274fdaf182b3a7d3b13
GerardoGala <ggala@hotmail.com>
2026-08-12 21:28:22 +0800

    back to dashboard

---
+++ js/classes.js
@@ -0,0 +1,12 @@
+const dinghy101Link =
+    document.getElementById("dinghy101Link");
+
+
+
+        // ---------------------------------------------
+        // Open Dinghy Sailing 101
+        // ---------------------------------------------
+
+        dinghy101Link.href =
+           "https://gerardogala.github.io/DinghySailing101/" +
+            "?studentEmail=0";
