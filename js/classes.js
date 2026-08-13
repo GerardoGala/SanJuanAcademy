f7889138939fc98c70eadaebec9c19e75e6bfe1d
GerardoGala <ggala@hotmail.com>
2026-08-13 11:21:20 +0800

    Improved Classes and Program

--- js/classes.js
+++ js/classes.js
@@ -1,26 +1,14 @@
-const dinghy101ProgramLink =
-    document.getElementById("dinghy101ProgramLink");
-
-dinghy101ProgramLink.href =
-    "https://gerardogala.github.io/SanJuanAcademy/program.html?class=dinghy101";
+const dinghy101ProgramLink = document.getElementById("dinghy101ProgramLink");
+dinghy101ProgramLink.href = "program.html?class=dinghy101";
 
 
-const dinghy101SyllabusLink =
-    document.getElementById("dinghy101SyllabusLink");
-
-dinghy101SyllabusLink.href =
-    "https://gerardogala.github.io/DinghySailing101/";
+const dinghy101SyllabusLink = document.getElementById("dinghy101SyllabusLink");
+dinghy101SyllabusLink.href = "https://gerardogala.github.io/DinghySailing101/";
 
 
-const dinghy201ProgramLink =
-    document.getElementById("dinghy201ProgramLink");
-
-dinghy201ProgramLink.href =
-    "https://gerardogala.github.io/SanJuanAcademy/program.html?class=dinghy201";
+const dinghy201ProgramLink = document.getElementById("dinghy201ProgramLink");
+dinghy201ProgramLink.href = "program.html?class=dinghy201";
 
 
-const dinghy201SyllabusLink =
-    document.getElementById("dinghy201SyllabusLink");
-
-dinghy201SyllabusLink.href =
-    "https://gerardogala.github.io/DinghySailing201/";
+const dinghy201SyllabusLink = document.getElementById("dinghy201SyllabusLink");
+dinghy201SyllabusLink.href = "https://gerardogala.github.io/DinghySailing201/";
