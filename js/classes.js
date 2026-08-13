f7b6210a8696a313e5609b6e6cd28c6e18b82704
GerardoGala <ggala@hotmail.com>
2026-08-13 10:23:10 +0800

    more code for classes and program

--- js/classes.js
+++ js/classes.js
@@ -1,12 +1,26 @@
-const dinghy101Link =
-    document.getElementById("dinghy101Link");
+const dinghy101ProgramLink =
+    document.getElementById("dinghy101ProgramLink");
+
+dinghy101ProgramLink.href =
+    "https://gerardogala.github.io/SanJuanAcademy/program.html?class=dinghy101";
 
 
+const dinghy101SyllabusLink =
+    document.getElementById("dinghy101SyllabusLink");
 
-        // ---------------------------------------------
-        // Open Dinghy Sailing 101
-        // ---------------------------------------------
+dinghy101SyllabusLink.href =
+    "https://gerardogala.github.io/DinghySailing101/";
 
-        dinghy101Link.href =
-           "https://gerardogala.github.io/DinghySailing101/" +
-            "?studentEmail=0";
+
+const dinghy201ProgramLink =
+    document.getElementById("dinghy201ProgramLink");
+
+dinghy201ProgramLink.href =
+    "https://gerardogala.github.io/SanJuanAcademy/program.html?class=dinghy201";
+
+
+const dinghy201SyllabusLink =
+    document.getElementById("dinghy201SyllabusLink");
+
+dinghy201SyllabusLink.href =
+    "https://gerardogala.github.io/DinghySailing201/";
